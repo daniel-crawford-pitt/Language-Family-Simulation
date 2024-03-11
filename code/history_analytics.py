c3dbb43857aadd46ae30d8f9be320e28c667b2ca
daniel-crawford-pitt <daniel.crawford@pitt.edu>
2024-03-11 17:30:15 -0400

    history analytics flow

--- code/history_analytics.py
+++ code/history_analytics.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from bigtree import dict_to_tree,list_to_tree, tree_to_dot,preorder_iter, hprint_tree
 from bigtree.utils.plot import reingold_tilford
-
+#root.show(attr_list=["x", "y","t_start"])
 import matplotlib as pyplot
 
 import matplotlib.pyplot as plt
@@ -14,30 +14,26 @@ def history_analysis(lhs, test_mode = False):
     for lh in lhs: print(lh)
     print('----------------------------------------------')
 
-    if test_mode:
-        n = ['t']
-        x = [1]
-        y = [1]
+    history_dict = parse_histories(lhs)
+    true_tree = dict_to_tree(history_dict)
+    hprint_tree(true_tree)
+    adjusted_history_dict = adjust_horizon(history_dict, 40)
+    adjusted_tree = dict_to_tree(adjusted_history_dict)
+    hprint_tree(adjusted_tree)
+    
+    
 
-    else: 
 
+def parse_histories(lhs):
         ts = []
         tree_hists = []
         for lh in lhs:
             tree_hists.append("INITIAL/"+'/'.join([c[-4:] for c in lh.split("+")]))
             ts.append(lh[:-1].split("_")[-2])
-        
-        
+               
         cdk_dict = {th:{'t_start':time} for th,time in zip(tree_hists,ts)}
 
-        print(cdk_dict)
-        root = dict_to_tree(cdk_dict)
-        #reingold_tilford(root)
-        #root.show(attr_list=["x", "y","t_start"])
-        root.show(attr_list=["t_start"])
-        #hprint_tree(root)
-
-        adjust_horizon(cdk_dict, 40)
+        return cdk_dict
 
 def adjust_horizon(cdk_dict, adjust_time):
     for v in cdk_dict.values():
